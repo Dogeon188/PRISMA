1d30e360c96d9a33c1a2215a5e9a866a0680a15a
Dogeon188 <dogcatdog188@gmail.com>
2024-06-03 02:17:18 +0800

    [Fix] toast manager initialize & call problem

--- assets/Scripts/Interface/ToastManager.ts
+++ assets/Scripts/Interface/ToastManager.ts
@@ -1,12 +1,13 @@
 import {
-    Component,
     Node,
     Prefab,
     _decorator,
     director,
     instantiate,
     log,
+    resources,
 } from "cc"
+import { EDITOR_NOT_IN_PREVIEW } from "cc/env"
 import { Toast } from "./Toast"
 const { ccclass, property } = _decorator
 
@@ -17,7 +18,7 @@ const { ccclass, property } = _decorator
  * Don't create it manually.
  */
 @ccclass("ToastManager")
-export class ToastManager extends Component {
+export class ToastManager {
     private static _inst: ToastManager = null
     private toastPool: Node[] = []
     private toastContainer: Node = null
@@ -32,20 +33,29 @@ export class ToastManager extends Component {
         return this._inst
     }
 
-    onLoad(): void {
+    constructor() {
         if (ToastManager._inst != null) {
             throw new Error(
                 "ToastManager is a singleton, don't create it twice.",
             )
         }
-        // Create a toast pool
-        for (let i = 0; i < 5; i++) {
-            const toast = instantiate(this.toastPrefab)
-            toast.parent = this.node
-            this.toastPool.push(toast)
-        }
-        director.addPersistRootNode(this.node)
-        this.toastContainer = this.node
+        this.toastContainer = new Node("__ToastManager__")
+        // Load the toast prefab
+        resources.load("Prefabs/Interface/Toast", Prefab, (err, prefab) => {
+            if (err) {
+                console.error("Failed to load toast prefab", err)
+                return
+            }
+            this.toastPrefab = prefab
+            console.log(prefab)
+            // Create a toast pool
+            for (let i = 0; i < 5; i++) {
+                const toast = instantiate(this.toastPrefab)
+                toast.parent = this.toastContainer
+                this.toastPool.push(toast)
+            }
+        })
+        director.addPersistRootNode(this.toastContainer)
     }
 
     show(message: string, duration: number = Toast.LENGTH_SHORT): void {
@@ -61,4 +71,11 @@ export class ToastManager extends Component {
         this.toastPool.push(toast)
         director.getScene().removeChild(toast)
     }
+}
+
+// initialize the singleton
+let initialized = false
+if (!EDITOR_NOT_IN_PREVIEW && !initialized) {
+    ToastManager.inst
+    initialized = true
 }
