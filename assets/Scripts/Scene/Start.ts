c94339fb8724081d8c1f81c76f24250087b3a51d
Dogeon188 <dogcatdog188@gmail.com>
2024-06-06 15:16:04 +0800

    [Add] logout at start

--- assets/Scripts/Scene/Start.ts
+++ assets/Scripts/Scene/Start.ts
@@ -1,6 +1,7 @@
-import { _decorator, AudioClip, Button, Component } from "cc"
+import { _decorator, AudioClip, Component } from "cc"
 import { AudioManager } from "../AudioManager"
 import { Auth } from "../Auth"
+import { ToastManager } from "../Interface/ToastManager"
 import { SceneManager } from "../SceneManager"
 const { ccclass, property } = _decorator
 
@@ -9,20 +10,8 @@ export class Start extends Component {
     @property(AudioClip)
     private bgm: AudioClip = null
 
-    @property(Button)
-    private startButton: Button = null
-
-    @property(Button)
-    private settingsButton: Button = null
-
     protected onLoad(): void {
         AudioManager.inst.fadeInBGM(this.bgm, 1)
-        this.startButton.node.on(Button.EventType.CLICK, this.startGame, this)
-        this.settingsButton.node.on(
-            Button.EventType.CLICK,
-            this.openSettings,
-            this,
-        )
     }
 
     protected startGame(): void {
@@ -33,25 +22,27 @@ export class Start extends Component {
     protected openSettings(): void {
         SceneManager.loadScene("Settings", true)
     }
+
+    protected logOut(): void {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => SceneManager.loadScene("Splash", true))
+            .catch((error) => {
+                ToastManager.show("Sign-out failed.")
+                console.error(error)
+            })
+    }
+
     /**
      * Load the stage based on the stored user data.
      */
     private loadStageOnUserData(): void {
         switch (Auth.data.stage) {
-            case 0:
+            case -1: // debug
                 SceneManager.loadScene("LevelTest")
-                break
-            case 1:
+            default: // normal
                 SceneManager.loadScene("LevelLobby")
-                break
-            case 2:
-                SceneManager.loadScene("LevelLobby")
-                break
-            case 3:
-                SceneManager.loadScene("LevelLobby")
-                break
-            default:
-                SceneManager.loadScene("LevelTest")
                 break
         }
     }
