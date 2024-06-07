b5ea9d98df713a01bea81c45882b2b1fe8f94e7a
Aaron Chen <godpeople1st@gmail.com>
2024-06-08 00:03:16 +0800

    [Fix] bug

--- assets/Scripts/Game/Entities/PlayerHalo.ts
+++ assets/Scripts/Game/Entities/PlayerHalo.ts
@@ -357,8 +357,8 @@ export class PlayerHalo extends Component {
     }
 
     private loadGemOnUserData(): void {
-        this.colorNumDict[ColliderGroup.RED] = 1 //Auth.data.gemNum.red
-        this.colorNumDict[ColliderGroup.GREEN] = 1 //Auth.data.gemNum.green
-        this.colorNumDict[ColliderGroup.BLUE] = 1 //Auth.data.gemNum.blue
+        this.colorNumDict[ColliderGroup.RED] = Auth.data.gemNum.red
+        this.colorNumDict[ColliderGroup.GREEN] = Auth.data.gemNum.green
+        this.colorNumDict[ColliderGroup.BLUE] = Auth.data.gemNum.blue
     }
 }
